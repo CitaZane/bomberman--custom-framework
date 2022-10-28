54300520bf6d543399584362fde310df8bc42557
itskris1 <kkangro7@gmail.com>
2022-10-28 13:26:49 +0300

    game room

--- frontend/app.js
+++ frontend/app.js
@@ -2,3 +2,7 @@ import createRouter from "../framework/router";
 import router from "./router";
 
 createRouter(router);
+
+window.addEventListener("DOMContentLoaded", () => {
+    // make sure user can be on the game page only if he has a websocket connection
+})
