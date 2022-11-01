083fb490e35fac3db1533dd68b6e2d6d9184861a
vic86 <viktorahu@gmail.com>
2022-11-01 12:10:18 +0200

    New messages is shown up now.

--- frontend/components/ChatRoom.js
+++ frontend/components/ChatRoom.js
@@ -1,44 +1,52 @@
 /* @jsx jsx */
 
-import jsx from "../../framework/vDom/jsx"
+import jsx from "../../framework/vDom/jsx";
 import { store } from "../app";
 import { ws } from "../websocket";
 
 function sendMessage(e) {
-    e.preventDefault();
+  e.preventDefault();
 
-    const formInput = e.target.elements.message;
+  const formInput = e.target.elements.message;
 
-    const msg = {
-        type: "TEXT_MESSAGE",
-        body: formInput.value,
-    }
+  const msg = {
+    type: "TEXT_MESSAGE",
+    body: formInput.value,
+  };
 
-    ws.send(JSON.stringify(msg));
+  ws.send(JSON.stringify(msg));
 
-    formInput.value = "";
+  formInput.value = "";
 }
 
 export function ChatRoom() {
-    const messages = store.state.messages
-    console.log("messages:",messages)
-    
-    return {
-        template: (
-            <div id="chatroom">
-                <header>
-                    <h2>Chat room</h2>
-                    <p>Connected as: John Doe</p>
-                </header>
+  const messages = store.state.messages;
+//   console.log("messages:", messages);
 
-                <div id="chat">
-                    <p>{messages}</p>
-                </div>
-                <form id="send-message" onSubmit={sendMessage}>
-                    <input type="text" name="message" placeholder="Write a message..."></input>
-                    <button>Send</button>
-                </form>
-            </div>
-        )
-    }
+  return {
+    template: (
+      <div id="chatroom">
+        <header>
+          <h2>Chat room</h2>
+          <p>Connected as: John Doe</p>
+        </header>
+
+        <div id="chat">
+          <ul>
+            {messages.map((message) => {
+              return <p>{message}</p>;
+            })}
+          </ul>
+        </div>
+        <form id="send-message" onSubmit={sendMessage}>
+          <input
+            type="text"
+            name="message"
+            placeholder="Write a message..."
+          ></input>
+          <button>Send</button>
+        </form>
+      </div>
+    ),
+  };
 }
