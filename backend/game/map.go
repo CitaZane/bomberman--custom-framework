af138a59d87ba4a0d5caf9143948694e24060689
vic86 <viktorahu@gmail.com>
2022-11-07 12:39:39 +0200

    base map with random breakables

--- backend/game/map.go
+++ backend/game/map.go
@@ -6,23 +6,23 @@ import "math/rand"
 // 0 is floor tile
 // 1 is breakable block
 // 2 is wall
-func CreateBaseMap() []int {
-	var m = make([]int, 0)
-	i := 0
-	for i < 121 {
-		if i < 11 || i > 109 || i%11 == 0 || i == 10 || i == 21 || i == 32 || i == 43 || i == 54 || i == 65 || i == 76 || i == 87 || i == 98 || i == 109 || i == 24 || i == 26 || i == 28 || i == 30 || i == 46 || i == 48 || i == 50 || i == 52 || i == 68 || i == 70 || i == 72 || i == 74 || i == 90 || i == 92 || i == 94 || i == 96 {
-			m = append(m, 2)
-		} else {
-			if rand.Intn(10) > 5 {
-				m = append(m, 0)
-			} else {
-				m = append(m, 1)
-			}
-		}
-		i++
-	}
-	return m
-}
+// func CreateBaseMap() []int {
+// 	var m = make([]int, 0)
+// 	i := 0
+// 	for i < 121 {
+// 		if i < 11 || i > 109 || i%11 == 0 || i == 10 || i == 21 || i == 32 || i == 43 || i == 54 || i == 65 || i == 76 || i == 87 || i == 98 || i == 109 || i == 24 || i == 26 || i == 28 || i == 30 || i == 46 || i == 48 || i == 50 || i == 52 || i == 68 || i == 70 || i == 72 || i == 74 || i == 90 || i == 92 || i == 94 || i == 96 {
+// 			m = append(m, 2)
+// 		} else {
+// 			if rand.Intn(10) > 5 {
+// 				m = append(m, 0)
+// 			} else {
+// 				m = append(m, 1)
+// 			}
+// 		}
+// 		i++
+// 	}
+// 	return m
+// }
 
 /* ---------------------- maybe base map could help -> ---------------------- */
 // predifined walls, empty corners for monsters and first breakable walls around monsters
@@ -42,17 +42,31 @@ func CreateBaseMap() []int {
 // 	2,2,2,2,2,2,2,2,2,2,2,
 // }
 
-// map withouth 3s look like this-> 
-// var mapBase = []int{
-// 	2,2,2,2,2,2,2,2,2,2,2,
-// 	2,0,0,1,.,.,.,1,0,0,2,
-// 	2,0,2,.,2,.,2,.,2,0,2,
-// 	2,1,.,.,.,.,.,.,.,1,2,
-// 	2,.,2,.,2,.,2,.,2,.,2,
-// 	2,.,.,.,.,.,.,.,.,.,2,
-// 	2,.,2,.,2,.,2,.,2,.,2,
-// 	2,1,.,.,.,.,.,.,.,1,2,
-// 	2,0,2,.,2,.,2,.,2,0,2,
-// 	2,0,0,1,.,.,.,1,0,0,2,
-// 	2,2,2,2,2,2,2,2,2,2,2,
-// }
+// map withouth 3s look like this->
+var mapBase = []int{
+	2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+	2, 0, 0, 1, 9, 9, 9, 1, 0, 0, 2,
+	2, 0, 2, 9, 2, 9, 2, 9, 2, 0, 2,
+	2, 1, 9, 9, 9, 9, 9, 9, 9, 1, 2,
+	2, 9, 2, 9, 2, 9, 2, 9, 2, 9, 2,
+	2, 9, 9, 9, 9, 9, 9, 9, 9, 9, 2,
+	2, 9, 2, 9, 2, 9, 2, 9, 2, 9, 2,
+	2, 1, 9, 9, 9, 9, 9, 9, 9, 1, 2,
+	2, 0, 2, 9, 2, 9, 2, 9, 2, 0, 2,
+	2, 0, 0, 1, 9, 9, 9, 1, 0, 0, 2,
+	2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+}
+
+func CreateBaseMap() []int {
+	basemap := mapBase
+	for i, tile := range basemap {
+		if tile == 9 {
+			if rand.Intn(10) > 5 {
+				mapBase[i] = 1
+			} else {
+				mapBase[i] = 0
+			}
+		}
+	}
+	return mapBase
+}
