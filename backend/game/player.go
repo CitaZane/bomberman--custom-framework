83ca666c3f01cb8714a92ba67f085a3f63c4b015
vic86 <viktorahu@gmail.com>
2022-11-10 17:17:24 +0200

    Some auto correction a player coordinates when he tryes go around a corner

--- backend/game/player.go
+++ backend/game/player.go
@@ -44,8 +44,6 @@ func CreatePlayer(name string, index int) Player {
 
 // methods for updating monster position based on input from websocket
 func (player *Player) Move(input string) {
-	// fmt.Println(player.calcPlayerPosition())
-	player.calcPlayerPosition()
 	// update movement variable
 	player.Movement = translateMovement(input)
 
@@ -62,116 +60,74 @@ func (player *Player) Move(input string) {
 
 // Movement functions
 func (player *Player) MoveUp() {
-	player.showCoordinates()
-	if player.Y == 64 {
-		return
-	}
-	// if (player.X-2)%64 == 0 || (player.X-1)%64 == 0 || player.X%64 == 0 || (player.X+1)%64 == 0 || (player.X+2)%64 == 0 {
-	if xFit(player) {
+	// player.showCoordinates()
+	xFit(player)
+	if player.X%64 == 0 {
 		if State.Map[player.calcPlayerPosition()] == 1 || State.Map[player.calcPlayerPosition()] == 2 {
 			return
+		} else {
+			player.Y -= player.Speed * 2
 		}
-		player.Y -= player.Speed
 	}
 }
 func (player *Player) MoveDown() {
-	player.showCoordinates()
-	// if (player.X-2)%64 == 0 || (player.X-1)%64 == 0 || player.X%64 == 0 || (player.X+1)%64 == 0 || (player.X+2)%64 == 0 {
-	if xFit(player) {
-
+	// player.showCoordinates()
+	xFit(player)
+	if player.X%64 == 0 {
 		if State.Map[player.calcPlayerPosition()+11] == 1 || State.Map[player.calcPlayerPosition()+11] == 2 {
 			return
-		}
-		player.Y += player.Speed
-	}
-}
-
-func xFit(player *Player) bool {
-	i := 5
-	for i > -5 {
-		if !((player.X-i)%64 == 0) {
-			i--
-			continue
 		} else {
-			return true
+			player.Y += player.Speed * 2
 		}
 	}
-	return false
-}
-func yFit(player *Player) bool {
-	i := 5
-	for i > -5 {
-		if !((player.Y-i)%64 == 0) {
-			i--
-			continue
-		} else {
-			return true
-		}
-	}
-	return false
 }
 
 func (player *Player) MoveRight() {
-	player.showCoordinates()
-	// if (player.Y-2)%64 == 0 || (player.Y-1)%64 == 0 || player.Y%64 == 0 || (player.Y+1)%64 == 0 || (player.Y+2)%64 == 0 {
-	if yFit(player) {
+	// player.showCoordinates()
+	yFit(player)
+	if player.Y%64 == 0 {
 		if State.Map[player.calcPlayerPosition()+1] == 1 || State.Map[player.calcPlayerPosition()+1] == 2 {
 			return
+		} else {
+			player.X += player.Speed * 2
 		}
-		player.X += player.Speed
 	}
-
-	// if player.X < 576 {
-	// 	player.X += player.Speed
-	// }
 }
 func (player *Player) MoveLeft() {
-	player.showCoordinates()
-	if player.X == 64 {
-		return
-	}
-	// fmt.Println("player pos", player.calcPlayerPosition())
-	// fmt.Println("index,val", player.calcPlayerPosition()-1, State.Map[player.calcPlayerPosition()-1])
-	// fmt.Println("MAP", State.Map)
-
-	// if (player.Y-2)%64 == 0 || (player.Y-1)%64 == 0 || player.Y%64 == 0 || (player.Y+1)%64 == 0 || (player.Y+2)%64 == 0 {
-	if yFit(player) {
+	yFit(player)
+	if player.Y%64 == 0 {
 		if State.Map[player.calcPlayerPosition()] == 1 || State.Map[player.calcPlayerPosition()] == 2 {
 			return
+		} else {
+			player.X -= player.Speed * 2
 		}
-		player.X -= player.Speed
 	}
-
-	// if player.X > 64 {
-	// 	player.X -= player.Speed
-	// }
 }
 
 // Calculates on which map cell player is standing. Cell is map index.
 func (player *Player) calcPlayerPosition() int {
-	// fmt.Println("player", player)
 	row := player.Y / 64
-	// fmt.Println("row", row)
 	place := player.X / 64
-	// fmt.Println("place", place)
-	// fmt.Println("row, place: ", row, place)
 	index := row*11 + place
-	// fmt.Println("index: ", index)
 	return index
 }
 
-// Check is there obstacles on the way.
-// func (player *Player) isBlockOnTheWay(gs *GameState) bool {
-
-// 	// fmt.Println("state map", gs.Map)
-// 	// fmt.Println("player x,y: ", player.X, player.Y)
-// 	// fmt.Println("map index, value: ", player.X/64+12, gs.Map[player.X/64+12])
-// 	if gs.Map[player.X/64+12] == 1 || gs.Map[player.X/64+12] == 2 {
-// 		return true
-// 	}
-// 	return false
-// }
-
 func (player *Player) showCoordinates() {
 	fmt.Printf("x: %v y: %v\n", player.X, player.Y)
+}
+
+func xFit(player *Player) {
+	if player.X%64 > 32 {
+		player.X++
+	} else {
+		player.X--
+	}
+}
+
+func yFit(player *Player) {
+	if player.Y%64 > 32 {
+		player.Y++
+	} else {
+		player.Y--
+	}
 }
