fe6cf759815c4ff3eb04a38b8aa475e419fdb018
vic86 <viktorahu@gmail.com>
2022-11-23 02:37:24 +0200

    timer added

--- frontend/components/Overlay.js
+++ frontend/components/Overlay.js
@@ -1,11 +1,19 @@
 import jsx from "../framework/vDom/jsx";
 
 export function Overlay() {
-
+    var timeleft = 10;
+    var intervalId = setInterval(function () {
+        if (timeleft <= 0) {
+            clearInterval(intervalId);
+            document.getElementById("overlay").style.visibility="hidden";
+        }
+        document.getElementById("overlay-counter").textContent = timeleft;
+        timeleft -= 1;
+    }, 1000);
     return {
         template: (
             <div id="overlay">
-                <div id="overlay-counter">10</div>
+                <div id="overlay-counter"></div>
             </div>
         ),
     };
